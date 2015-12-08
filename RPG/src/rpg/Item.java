21f2cfd69b33937b51f0fb39503cd7b1a86fe235
p1509413 <p1509413@POLYTECH25.univ-lyon1.fr>
2015-12-08 13:40:21 +0100

    Ajout du squelette de la classe Item

---
+++ RPG/src/rpg/Item.java
@@ -0,0 +1,14 @@
+/*
+ *  Projet - APO Cr�ation d'un RPG
+ *  Decembre 2015
+ *  Gaetan MARTIN & Jensen Joymangul
+ */
+package rpg;
+
+/**
+ * Classe Item : Représente un objet
+ * @author p1509413
+ */
+public class Item {
+    
+}
