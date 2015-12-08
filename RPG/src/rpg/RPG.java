45408b0dfae1419dd2411e59c9c01fe4e263804e
p1509413 <p1509413@POLYTECH25.univ-lyon1.fr>
2015-12-08 13:04:27 +0100

    Premier commit : Création du corps du projet

---
+++ RPG/src/rpg/RPG.java
@@ -0,0 +1,21 @@
+/*
+ * Projet - APO Création d'un RPG
+ * Décembre 2015
+ * Gaëtan MARTIN & Jensen Joymangul
+ */
+
+package rpg;
+
+/**
+ * Classe principale exécutable
+ * @author p1509413
+ */
+public class RPG {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+    
+}
