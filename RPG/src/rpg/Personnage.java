a7c8c96d3843f788b5b611d1e109fdb851bb90db
p1509413 <p1509413@POLYTECH25.univ-lyon1.fr>
2015-12-08 13:40:11 +0100

    Ajout du squelette de la classe Personnage

---
+++ RPG/src/rpg/Personnage.java
@@ -0,0 +1,15 @@
+/*
+ * Projet - APO Création d'un RPG
+ * Décembre 2015
+ * Gaëtan MARTIN & Jensen Joymangul
+ */
+
+package rpg;
+
+/**
+ * Classe Personnage : Représente un personnage du RPG
+ * @author p1509413
+ */
+public class Personnage {
+    
+}
