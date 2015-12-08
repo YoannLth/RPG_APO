7d208734bb39da8ae401ddc16ac9e6467a52989b
p1509413 <p1509413@POLYTECH25.univ-lyon1.fr>
2015-12-08 13:59:05 +0100

    Ajout de l'attribut carractéristiques

--- RPG/src/rpg/Personnage.java
+++ RPG/src/rpg/Personnage.java
@@ -6,10 +6,26 @@
 
 package rpg;
 
+import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Classe Personnage : Représente un personnage du RPG
  * @author p1509413
  */
 public class Personnage {
     
+    /**
+     * Carractéristiques du personnage
+     */
+    private Map<Caracteristique, Integer> carracs;
+    
+    /**
+     * Constructeur par défaut
+     */
+    public Personnage()
+    {
+        this.carracs = new HashMap<>();
+    }
 }
