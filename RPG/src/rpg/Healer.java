3a065491c6cb42e006f477b8bb80b20d890df803
Gaëtan MARTIN <gaetan.martin1993@gmail.com>
2016-01-14 19:56:27 +0100

    Update des items par défaut du healer

--- RPG/src/rpg/Healer.java
+++ RPG/src/rpg/Healer.java
@@ -17,6 +17,19 @@ public class Healer extends Character
     {
         super();
         this.setMaxHealth(MAX_HEALTH_HEALER);
+        this.initInventoryHealer();
     }
-;
+    
+    /**
+     * Init the inventory of the healer
+     */
+    private void initInventoryHealer()
+    {
+        super.inventory.add(new Edible("MinorHealthPotion", 10, true, new Effect(Ability.HEALTH, 50, 1)));
+        super.inventory.add(new Edible("MinorHealthPotion", 10, true, new Effect(Ability.HEALTH, 50, 1)));
+        super.inventory.add(new Edible("MinorHealthPotion", 10, true, new Effect(Ability.HEALTH, 50, 1)));
+        super.inventory.add(new Edible("MajorHealthPotion", 10, true, new Effect(Ability.HEALTH, 100, 1)));
+        super.inventory.add(new Edible("MajorHealthPotion", 10, true, new Effect(Ability.HEALTH, 100, 1)));
+        super.inventory.add(new Edible("MajorHealthPotion", 10, true, new Effect(Ability.HEALTH, 100, 1)));
+    }
 }
