e95cf4f1fb3defd3c093675448cd9bfd575dceb9
p1509413 <p1509413@POLYTECH50.univ-lyon1.fr>
2016-01-14 11:59:40 +0100

    Update de l'AI - On soigne ou on attaque la cible la plus basse en HP

--- RPG/src/Controller/ControllerAI.java
+++ RPG/src/Controller/ControllerAI.java
@@ -27,6 +27,11 @@ public class ControllerAI extends Controller
      * List of the ai Characters - Needed to chose a target (i.e : for a heal)
      */
     ArrayList<Character> aiCharacters;
+    
+    /**
+     * Indicates that an aiCharacter needs a heal
+     */
+    private boolean healNeeded = false;
 
     public ControllerAI(Character ai,
             ArrayList<Character> playerCharacters,
@@ -39,12 +44,46 @@ public class ControllerAI extends Controller
 
     /**
      * Get the action of the AI
-     * @return 
+     * @return the action of the AI
      */
     public Action getAction()
     {
-        // TODO : Implement this AI
+        Character target = this.choseTarget();        
+        
         return new Action(character, character, new Attack(character, character));
+    }
+
+    /**
+     * 
+     * @return 
+     */
+    private Character choseTarget()
+    {
+        if (healNeeded)
+        {
+            return getCharacterWithLowerHealth(aiCharacters);
+        }
+        else
+        {
+            return getCharacterWithLowerHealth(playerCharacters);
+        }
+    }
+
+    /**
+     * 
+     * 
+     * @param listCharacters
+     * @return the character with the lower health from the list given
+     */
+    private Character getCharacterWithLowerHealth(ArrayList<Character> listCharacters)
+    {
+        Character characterWithLowerHealth = listCharacters.get(0); // Init to the first
+        for(Character c : listCharacters)
+        {
+            if (characterWithLowerHealth.getCurrentHealth() <= c.getCurrentHealth())
+                characterWithLowerHealth = c;
+        }
+        return characterWithLowerHealth;
     }
 
 }
