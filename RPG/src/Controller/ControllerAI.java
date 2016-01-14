1071a6e039110739461ab06f6b7a542843f6a019
p1509413 <p1509413@POLYTECH50.univ-lyon1.fr>
2016-01-14 11:37:42 +0100

    Ajout du squelette de code pour les actions de l'AI

--- RPG/src/Controller/ControllerAI.java
+++ RPG/src/Controller/ControllerAI.java
@@ -5,18 +5,46 @@
  */
 package Controller;
 
+import java.util.ArrayList;
+import rpg.Action;
+import rpg.Attack;
 import rpg.Character;
 
 /**
  * Class managing the actions of the AI
+ *
  * @author p1509413
  */
 public class ControllerAI extends Controller
 {
 
-    public ControllerAI(Character c)
+    /**
+     * List of the player Characters - Needed to chose a target
+     */
+    ArrayList<Character> playerCharacters;
+
+    /**
+     * List of the ai Characters - Needed to chose a target (i.e : for a heal)
+     */
+    ArrayList<Character> aiCharacters;
+
+    public ControllerAI(Character ai,
+            ArrayList<Character> playerCharacters,
+            ArrayList<Character> aiCharacters)
     {
-        super(c);
+        super(ai);
+        this.playerCharacters = playerCharacters;
+        this.aiCharacters = aiCharacters;
     }
-    
+
+    /**
+     * Get the action of the AI
+     * @return 
+     */
+    public Action getAction()
+    {
+        // TODO : Implement this AI
+        return new Action(character, character, new Attack(character, character));
+    }
+
 }
