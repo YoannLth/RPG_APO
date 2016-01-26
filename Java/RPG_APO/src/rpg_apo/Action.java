1bbb5e123e4035f6fe5b0a3e92ec893d408d28a8
YoannLth <yoann.lathuiliere@gmail.com>
2016-01-26 22:36:28 +0100

    Action class commented

--- Java/RPG_APO/src/rpg_apo/Action.java
+++ Java/RPG_APO/src/rpg_apo/Action.java
@@ -1,12 +1,13 @@
 package rpg_apo;
 
 public class Action {
-
+        // An action is made of a target, a source, and a capacity or an item
 	private Character target;
 	private Character src;
 	private Capacity cap;
 	private ConsumableItem consum;
         
+        // Constructor for a capacity
         public Action( Character s, Character t, Capacity c){
             this.target=t;
             this.src=s;
@@ -14,13 +15,15 @@ public class Action {
             this.consum=null;
         }
         
+        // Contructor for an item
         public Action(Character t, Character s, ConsumableItem cons){
             this.target=t;
             this.src=s;
             this.cap=null;
             this.consum=cons;
         }
-
+        
+        // Function who return the script displayed action reading 
         public String readAction(){
             String actionDisplay = "";
             
@@ -57,9 +60,5 @@ public class Action {
             }
             return actionDisplay;
         }
-        
-        public void SetCapacity(){
-            
-        }
-        
+                
 }
